09aa146b01bbb277b0f1bafe3cd3c03abaee0051
Darien <dbouzakis@gmail.com>
2019-02-07 17:44:18 -0500

    adds loop for a home's pricing table

--- redfin_starter.py
+++ redfin_starter.py
@@ -18,44 +18,70 @@ driver.get("https://www.redfin.com/NJ/West-Deptford/877-Dante-Ct-08051/home/3649
 #estimate_button.click()
 
 
-
 print("Scraping page...")
 
-address = driver.find_elements_by_xpath('.//span[@class="street-address"]')[0].text 
 
+#csv_file = open('reviews.csv', 'w', encoding='utf-8')         #save every 500 rows of data, if a problem w script 
+#writer = csv.writer(csv_file)
+
+
+address = driver.find_elements_by_xpath('.//span[@class="street-address"]')[0].text 
 print(address)				#trying to print out the first part of list is sending garbage 
 
 price = driver.find_elements_by_xpath('.//span[@class="statsValue"]')[0].text
-
-print(price)
+#print(price)
 
 numbed = driver.find_elements_by_xpath('.//div[@class="statsValue"]')[0].text
-
-print(numbed) 
+#print(numbed) 
 
 
 #county = driver.find_elements_by_xpath('//*[@id="basicInfo"]/div[2]/div[1]/div[11]/div')
-county = driver.find_elements_by_xpath('.//div[@class="table-value"]')[0].text
-
-print(county) #printing a '-' instead of county 
+#county = driver.find_elements_by_xpath('.//div[@class="table-value"]')[0].text
+#print(county) #printing a '-' instead of county 
 
 
 mls = driver.find_elements_by_xpath('.//span[@class="header font-color-gray-light font-weight-roman"]')[0].text  
-
 print(mls)   # printing 'Style' instead of MLS #
 
 price1 = driver.find_elements_by_xpath('.//td[@class="price-col number"]')[0].text  # output: price1 is $110,000
-
-print(price1)  
+#print(price1)  
 
 price2 = driver.find_element_by_xpath('.//td[@class="price-col number"]').text    # output is $110,000 price2 is a part of a table, can this be printed?
-print(price2)
+#print(price2)
 
 print('*' * 50) 
 
 
-driver.close() 
+
+price2 = driver.find_element_by_xpath('//*[@id="propertyHistory-expandable-segment"]/div[1]/div/table').text  
+
+#click before table 
+button = driver.find_element_by_xpath('.//span[@class="bottomLink font-color-link"]')
+button.click()
 
 
+
+#table = driver.find_element_by_xpath('.//table[@class="basic-table-2"]').text 
+#print(table)
+i=0
+
+colum ={} 
+
+while i>=0: 
+
+	colum = driver.find_elements_by_xpath('.//tr[@class=" PropertyHistoryEventRow"]')[i].text
+
+	#colum = driver.find_element_by_css_selector("td:nth-child(3)").text
+
+	colum['price'] = colum
+	i += 1 
+	print(i) 
+	print(colum)
+
+	if i == 7: 
+		break 
+		driver.close()
+
+ 
 
 #git hub line for errors 
