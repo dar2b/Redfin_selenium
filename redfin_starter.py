66cfb59b4bb449d1d8518fd6b76e2720e840b30a
Darien <dbouzakis@gmail.com>
2019-02-07 20:22:31 -0500

    loop with writing to csv

--- redfin_starter.py
+++ redfin_starter.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import csv
 
 # Windows users need to specify the path to chrome driver you just downloaded.
 # You need to unzip the zipfile first and move the .exe file to any folder you want.
@@ -21,24 +22,20 @@ driver.get("https://www.redfin.com/NJ/West-Deptford/877-Dante-Ct-08051/home/3649
 print("Scraping page...")
 
 
-#csv_file = open('reviews.csv', 'w', encoding='utf-8')         #save every 500 rows of data, if a problem w script 
-#writer = csv.writer(csv_file)
+csv_file = open('pricing.csv', 'w', encoding='utf-8')         #save every 500 rows of data, if a problem w script 
+writer = csv.writer(csv_file)
 
 
 address = driver.find_elements_by_xpath('.//span[@class="street-address"]')[0].text 
-print(address)				#trying to print out the first part of list is sending garbage 
-
-price = driver.find_elements_by_xpath('.//span[@class="statsValue"]')[0].text
+print(address)			
+#price = driver.find_elements_by_xpath('.//span[@class="statsValue"]')[0].text
 #print(price)
-
-numbed = driver.find_elements_by_xpath('.//div[@class="statsValue"]')[0].text
+#numbed = driver.find_elements_by_xpath('.//div[@class="statsValue"]')[0].text
 #print(numbed) 
-
 
 #county = driver.find_elements_by_xpath('//*[@id="basicInfo"]/div[2]/div[1]/div[11]/div')
 #county = driver.find_elements_by_xpath('.//div[@class="table-value"]')[0].text
 #print(county) #printing a '-' instead of county 
-
 
 mls = driver.find_elements_by_xpath('.//span[@class="header font-color-gray-light font-weight-roman"]')[0].text  
 print(mls)   # printing 'Style' instead of MLS #
@@ -46,42 +43,41 @@ print(mls)   # printing 'Style' instead of MLS #
 price1 = driver.find_elements_by_xpath('.//td[@class="price-col number"]')[0].text  # output: price1 is $110,000
 #print(price1)  
 
-price2 = driver.find_element_by_xpath('.//td[@class="price-col number"]').text    # output is $110,000 price2 is a part of a table, can this be printed?
+#price2 = driver.find_element_by_xpath('.//td[@class="price-col number"]').text    # output is $110,000 price2 is a part of a table, can this be printed?
 #print(price2)
 
 print('*' * 50) 
 
-
-
-price2 = driver.find_element_by_xpath('//*[@id="propertyHistory-expandable-segment"]/div[1]/div/table').text  
+#price2 = driver.find_element_by_xpath('//*[@id="propertyHistory-expandable-segment"]/div[1]/div/table').text  
 
 #click before table 
 button = driver.find_element_by_xpath('.//span[@class="bottomLink font-color-link"]')
 button.click()
 
-
-
-#table = driver.find_element_by_xpath('.//table[@class="basic-table-2"]').text 
+table = driver.find_element_by_xpath('.//table[@class="basic-table-2"]').text 
 #print(table)
-i=0
 
-colum ={} 
+for ele in table:
+	i=0
+	while i>=0: 
+		#dictionary to intilize for file
+		colum = {}  
 
-while i>=0: 
+		colum1 = driver.find_elements_by_xpath('.//tr[@class=" PropertyHistoryEventRow"]')[i].text
+		#colum = driver.find_element_by_css_selector("td:nth-child(3)").text
+		
+		i+=1 #counter 
 
-	colum = driver.find_elements_by_xpath('.//tr[@class=" PropertyHistoryEventRow"]')[i].text
+		colum['price']=colum1 
 
-	#colum = driver.find_element_by_css_selector("td:nth-child(3)").text
-
-	colum['price'] = colum
-	i += 1 
-	print(i) 
-	print(colum)
-
-	if i == 7: 
+		print(i) 
+		print(colum)
+		writer.writerow(colum.values())
+	
+	if i == 9: 									#  set at 8 for each column 
 		break 
 		driver.close()
-
+	break 
  
 
 #git hub line for errors 
