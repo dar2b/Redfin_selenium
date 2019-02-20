2fc292f306c7d4970bdbba7333e2b7e819e2ed1e
Darien <dbouzakis@gmail.com>
2019-02-20 18:31:00 -0500

    the alligned column headers along with the Price column

--- redfin_details_page.py
+++ redfin_details_page.py
@@ -14,37 +14,42 @@ list_of_search_result_urls = list(pd.read_csv('search_results.csv')['url'])
 # create a file for details page scraping
 csv_file = open('details_page_info.csv', 'w')
 writer = csv.writer(csv_file)
-details_page_info_headers = ['Price', 'Beds', 'Bath']
+details_page_info_headers = ['Price', 'Beds', 'Bath']		## Headers are not alligning with columns. Why? 
 writer.writerow(details_page_info_headers)
 
 for url in list_of_search_result_urls:
 	# Go to the page that we want to scrape					#other values first then table 
 	driver.get(url)
 
-	asset_estate={}
+	resi_estate={}
 	product_price = driver.find_element_by_xpath("//div[@class='info-block price']").text
-	price = product_price.strip('b \n $ Price')    #remove $ ... how do you remove " "  around price ?
-	print('\n' + price)
-	asset_estate['price'] = price       #.encode('utf-8')
+	resi_price = product_price.strip('b \n $ Price')    #remove $ ... how do you remove " "  around price ?
+	print('\n' + resi_price)
+	resi_estate['Price'] = resi_price       #.encode('utf-8')
 	
 	bed_dict={}
 	bed = driver.find_element_by_xpath("//div[@class='info-block']/div[@class='statsValue']").text
 	print('Bed ' + bed)
-	asset_estate['bed'] = bed
+	resi_estate['Beds'] = bed
 
 	bath = driver.find_element_by_xpath("//div[@data-rf-test-id='abp-baths']/div[@class='statsValue']").text
 	print('Bath ' + bath) 
-	asset_estate['bath'] = bath
+	resi_estate['bath'] = bath
 
 	sq_feet = driver.find_element_by_xpath("//div[@class='info-block sqft']").text		
 	print(sq_feet)
-	asset_estate['sq_feet'] = sq_feet
+	resi_estate['sq_feet'] = sq_feet
 
 
 
 	###### historical pricing or time series -- delete from here down to use script ######
 	#button = driver.find_element_by_xpath('.//span[@class="bottomLink font-color-link"]')		#button erroring when not avaiable to click on first page 
 	#button.click()
+
+
+	## Need MLS ID for primary key 
+	#mls_id = driver.find_elements_by_xpath("//span[@class='content font-weight-roman']").text    span[@class='header font-color-gray-light font-weight-roman']/ .. taking the wrong class 
+	#print(mls_id)
 
 	table = driver.find_element_by_xpath('.//table[@class="basic-table-2"]').text      
 	print(table)
@@ -61,15 +66,21 @@ for url in list_of_search_result_urls:
 	#acres_num = driver.find_elements_by_xpath("//span[@class='content font-weight-roman']")[5].text 
 	#print(acres_num)
 	
-	## Need MLS ID for primary key 
-	#mls_id = driver.find_elements_by_xpath("//span[@class='content font-weight-roman']").text    span[@class='header font-color-gray-light font-weight-roman']/ .. taking the wrong class 
-	#print(mls_id)
-	writer.writerow(asset_estate.values())
-	#writer.writerow(bed_dict.values() )       ## This should be the second column as bed, it is formatting below each other 
+	
+	writer.writerow(resi_estate.values()) 
 
 
+	
+##cap rate = noi / value 
 
 
+###20,000.00	purchase price
+##11,000.00	5% down payment
+##6,600.00	3% closing costs
+##17,600.00	cash at closing
+
+
+### sum and scrap loan information for debt serviced 
 ##pd.Dataframe(price_dict)
 
 
