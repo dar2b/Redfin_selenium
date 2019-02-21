d24891f27c2fd35f862f857d0563194a21bf5797
Darien <dbouzakis@gmail.com>
2019-02-21 13:44:38 -0500

    table formatting. Table and MLS ID being worked on

--- redfin_details_page.py
+++ redfin_details_page.py
@@ -14,22 +14,36 @@ list_of_search_result_urls = list(pd.read_csv('search_results.csv')['url'])
 # create a file for details page scraping
 csv_file = open('details_page_info.csv', 'w')
 writer = csv.writer(csv_file)
-details_page_info_headers = ['Price', 'Beds', 'Bath', 'Square feet']		## Headers are not alligning with only the juypter notebook. Why? 
-writer.writerow(details_page_info_headers)					## The counties should be compared. Should I have county data included and seperated for the dataset(s)?
+details_page_info_headers = ['Price', 'Beds', 'Bath', 'Square feet', '$ per Sq. Ft.', 'County']	 
+writer.writerow(details_page_info_headers)								## The counties should be compared. Should I have county data included and seperated for the dataset(s)? Included 'county'.
 
 for url in list_of_search_result_urls:
-	# Go to the page that we want to scrape					#other values first then table 
+	# Go to the page that we want to scrape								#other values first then table 
 	driver.get(url)
 
 	resi_estate={}
+	
+	try: 
+		## Need MLS ID for primary key 
+		button = driver.find_element_by_xpath('.//span[@class="bottomLink font-color-link"]')		              ## MLS ID should come first in script 
+		button.click()
+		mls_id = driver.find_elements_by_xpath("//div[@class='source-info font-b2 font-color-gray-light']").text    #span[@class='header font-color-gray-light font-weight-roman']/ .. taking the wrong class 
+		print(mls_id)
+	except:
+		pass  
+
+
+
 	product_price = driver.find_element_by_xpath("//div[@class='info-block price']").text
-	resi_price = product_price.strip('b \n $ Price')    #remove $ ... how do you remove " "  around price ?
+
+	resi_price = product_price.strip('b \n $ Price')    						#remove $ ... how do you remove " "  around price ?
 	resi_price_ = resi_price.strip(' \n Listed at price ')
 	resi_price_2 = resi_price_.replace(',', '')
+
 	int_resi_price = resi_price_2
 
 	print('\n' + int_resi_price)
-	resi_estate['Price'] = int_resi_price       #.encode('utf-8')
+	resi_estate['Price'] = int_resi_price       			#.encode('utf-8')
 	
 	bed_dict={}
 	bed = driver.find_element_by_xpath("//div[@class='info-block']/div[@class='statsValue']").text
@@ -41,10 +55,37 @@ for url in list_of_search_result_urls:
 	resi_estate['bath'] = bath
 
 	sq_feet = driver.find_element_by_xpath("//div[@class='info-block sqft']").text	
-	sq_feet.split(' ,' )															## how do you seperate the string and make it into two variables? 
-	print(sq_feet)
-	resi_estate['sq_feet'] = sq_feet
 
+	sq_feet_split = sq_feet.split('$' )											## how do you seperate the string and make it into two variables? Removed the dollar sign. 
+	square_footage = sq_feet_split[0] 
+	square_foot_dollar = sq_feet_split[1]
+	square_foot_total = square_footage.replace('Sq. Ft.' , '')					#.strip was not working on this function. Why? 
+	square_foot_noline =  square_foot_total.strip(' \n')
+	square_foot_comma = square_foot_noline.replace(',' ,'')	
+	dollar_per = square_foot_dollar.strip('/ Sq. Ft.')	
+
+	print(square_foot_comma, dollar_per)
+	resi_estate['sq feet'] = square_foot_comma
+	resi_estate['sq_feet_per_$'] = dollar_per
+
+
+	###### historical pricing or time series -- delete from here down to use script ######
+	try: 
+		button = driver.find_element_by_xpath('.//span[@class="bottomLink font-color-link"]')		              
+		button.click()																					
+		table = driver.find_element_by_xpath('.//table[@class="basic-table-2"]').text      
+		print(table)
+
+		no_button_table = driver.find_element_by_xpath('.//tr[@class="PropertyHistoryEventRow"]') 			##This xpath is not taking the home's pricing information that does not have a full table or button
+		print(no_button_table)
+	except:
+		pass  
+
+
+
+	county_type = driver.find_element_by_xpath("//span[@class='content font-weight-roman']/a[@href]").text	
+	print(county_type)
+	resi_estate['county'] = county_type
 
 
 
@@ -61,22 +102,6 @@ for url in list_of_search_result_urls:
 	
 	#plt.scatter(list_of_asset_details, list_of_asset_details)
 
-	###### historical pricing or time series -- delete from here down to use script ######
-	##try: 
-	#	button = driver.find_element_by_xpath('.//span[@class="bottomLink font-color-link"]')		               #button erroring when not avaiable to click on first page 
-	#	button.click()
-
-	#except:
-	#	pass    
-
-
-
-	## Need MLS ID for primary key 
-	#mls_id = driver.find_elements_by_xpath("//span[@class='content font-weight-roman']").text    #span[@class='header font-color-gray-light font-weight-roman']/ .. taking the wrong class 
-	#print(mls_id)
-
-	table = driver.find_element_by_xpath('.//table[@class="basic-table-2"]').text      
-	print(table)
 
 
 	#####real esate historical pricing 
